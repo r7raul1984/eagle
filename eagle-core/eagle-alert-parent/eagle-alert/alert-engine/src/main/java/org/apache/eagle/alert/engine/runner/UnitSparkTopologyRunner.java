164ac5f53a58b84d06537b938a2ba99679ac6d5c
r7raul1984 <tangjijun@yhd.com>
2016-08-10 10:10:42 +0800

    add hack chang get config method

--- eagle-core/eagle-alert-parent/eagle-alert/alert-engine/src/main/java/org/apache/eagle/alert/engine/runner/UnitSparkTopologyRunner.java
+++ eagle-core/eagle-alert-parent/eagle-alert/alert-engine/src/main/java/org/apache/eagle/alert/engine/runner/UnitSparkTopologyRunner.java
@@ -16,25 +16,25 @@
  */
 package org.apache.eagle.alert.engine.runner;
 
-import com.google.common.base.Preconditions;
 import com.typesafe.config.Config;
 import kafka.common.TopicAndPartition;
 import kafka.message.MessageAndMetadata;
 import kafka.serializer.StringDecoder;
+import org.apache.eagle.alert.coordination.model.AlertBoltSpec;
+import org.apache.eagle.alert.coordination.model.PublishSpec;
+import org.apache.eagle.alert.coordination.model.SpoutSpec;
 import org.apache.eagle.alert.engine.coordinator.IMetadataChangeNotifyService;
-import org.apache.eagle.alert.engine.spark.function.*;
+import org.apache.eagle.alert.engine.coordinator.StreamDefinition;
+import org.apache.eagle.alert.service.SpecMetadataServiceClientImpl;
 import org.apache.spark.SparkConf;
-import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.function.Function;
+import org.apache.spark.function.*;
 import org.apache.spark.streaming.Durations;
 import org.apache.spark.streaming.api.java.JavaInputDStream;
 import org.apache.spark.streaming.api.java.JavaPairDStream;
 import org.apache.spark.streaming.api.java.JavaStreamingContext;
-import org.apache.spark.streaming.kafka.HasOffsetRanges;
-import org.apache.spark.streaming.kafka.KafkaCluster;
-import org.apache.spark.streaming.kafka.KafkaUtils;
-import org.apache.spark.streaming.kafka.OffsetRange;
+import org.apache.spark.streaming.kafka.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import scala.Predef;
@@ -51,11 +51,14 @@ public class UnitSparkTopologyRunner implements Serializable {
     //kafka config
     private KafkaCluster kafkaCluster = null;
     private Map<String, String> kafkaParams = new HashMap<String, String>();
-    private final static String CONSUMER_KAFKA_TOPIC = "topology.topics";
-    private Set<String> topics = new HashSet<String>();
-    private java.util.Map<kafka.common.TopicAndPartition, Long> fromOffsets = new java.util.HashMap<kafka.common.TopicAndPartition, Long>();
+    private Map<TopicAndPartition, Long> fromOffsets = new HashMap<TopicAndPartition, Long>();
     private final AtomicReference<OffsetRange[]> offsetRanges = new AtomicReference<OffsetRange[]>();
-
+    private final AtomicReference<SpoutSpec> spoutSpecRef = new AtomicReference<SpoutSpec>();
+    private final AtomicReference<Map<String, StreamDefinition>> sdsRef = new AtomicReference<Map<String, StreamDefinition>>();
+    private final AtomicReference<AlertBoltSpec> alertBoltSpecRef = new AtomicReference<AlertBoltSpec>();
+    private final AtomicReference<PublishSpec> publishSpecRef = new AtomicReference<PublishSpec>();
+    private final AtomicReference<Set<String>> topicsRef = new AtomicReference<Set<String>>();
+    private String zkServers = null;//Zookeeper server string: host1:port1[,host2:port2,...]
     //spark config
     private final static String WINDOW_SECOND = "topology.window";
     private final static int DEFAULT_WINDOW_SECOND = 2;
@@ -102,13 +105,9 @@ public class UnitSparkTopologyRunner implements Serializable {
                     }
                 });
         this.kafkaCluster = new KafkaCluster(immutableKafkaParam);
-        String topic = config.getString(CONSUMER_KAFKA_TOPIC);
-        this.topics.add("oozie");
-
-
         this.config = config;
         this.topologyId = config.getString("topology.name");
-
+        this.zkServers = config.getString("zkConfig.zkQuorum");
         this.window = config.hasPath(WINDOW_SECOND) ? config.getLong(WINDOW_SECOND) : DEFAULT_WINDOW_SECOND;
         SparkConf sparkConf = new SparkConf();
         sparkConf.setAppName(topologyId);
@@ -142,10 +141,10 @@ public class UnitSparkTopologyRunner implements Serializable {
 
     private void buildTopology(JavaStreamingContext jssc, Config config) {
 
+        Set<String> topics = getInitTopics(config);
+        EagleKafkaUtils.fillInLatestOffsets(topics,this.fromOffsets,this.groupId,this.kafkaCluster,this.zkServers);
 
-        fillInLatestOffsets();
-
-        int windowDurations = config.getInt(WINDOW_DURATIONS);
+       int windowDurations = config.getInt(WINDOW_DURATIONS);
         int numOfRouter = config.getInt(ROUTER_TASK_NUM);
         int numOfAlertBolts = config.getInt(ALERT_TASK_NUM);
         int numOfPublishTasks = config.getInt(PUBLISH_TASK_NUM);
@@ -154,55 +153,45 @@ public class UnitSparkTopologyRunner implements Serializable {
         @SuppressWarnings("unchecked")
         Class<MessageAndMetadata<String, String>> streamClass =
                 (Class<MessageAndMetadata<String, String>>) (Class<?>) MessageAndMetadata.class;
-        JavaInputDStream<MessageAndMetadata<String, String>> messages = KafkaUtils.createDirectStream(jssc,
+        JavaInputDStream<MessageAndMetadata<String, String>> messages = EagleKafkaUtils.createDirectStream(jssc,
                 String.class, String.class, StringDecoder.class,
                 StringDecoder.class, streamClass, kafkaParams,
                 this.fromOffsets, message -> message);
         JavaPairDStream<String, String> pairDStream = messages.transform(new Function<JavaRDD<MessageAndMetadata<String, String>>, JavaRDD<MessageAndMetadata<String, String>>>() {
+
+            SpecMetadataServiceClientImpl client = new SpecMetadataServiceClientImpl(config);
+
             @Override
             public JavaRDD<MessageAndMetadata<String, String>> call(JavaRDD<MessageAndMetadata<String, String>> rdd) throws Exception {
+
+                spoutSpecRef.set(client.getSpoutSpec());
+                sdsRef.set(client.getSds());
+                alertBoltSpecRef.set(client.getAlertBoltSpec());
+                publishSpecRef.set(client.getPublishSpec());
+                topicsRef.set(getTopics(client));
+
                 OffsetRange[] offsets = ((HasOffsetRanges) rdd.rdd()).offsetRanges();
                 offsetRanges.set(offsets);
                 return rdd;
             }
+
+            private Set<String> getTopics(SpecMetadataServiceClientImpl client) {
+                return client.getSpoutSpec().getKafka2TupleMetadataMap().keySet();
+            }
         }).mapToPair(km -> new Tuple2<>(km.topic(), km.message()));
         pairDStream.window(Durations.seconds(windowDurations), Durations.seconds(windowDurations))
-                .flatMapToPair(new CorrelationSpoutSparkFunction(numOfRouter, config))
+                .flatMapToPair(new CorrelationSpoutSparkFunction(numOfRouter, spoutSpecRef, sdsRef))
                 .transformToPair(new ChangePartitionTo(numOfRouter))
                 .mapPartitionsToPair(new StreamRouteBoltFunction(config, "streamBolt"))
                 .transformToPair(new ChangePartitionTo(numOfAlertBolts))
-                .mapPartitionsToPair(new AlertBoltFunction(alertBoltNamePrefix, config, numOfAlertBolts))
-                .repartition(numOfPublishTasks).foreachRDD(new Publisher(config, alertPublishBoltName, kafkaCluster, groupId, offsetRanges));
+                .mapPartitionsToPair(new AlertBoltFunction(alertBoltNamePrefix, sdsRef, alertBoltSpecRef, numOfAlertBolts))
+                .repartition(numOfPublishTasks)
+                .foreachRDD(new Publisher(publishSpecRef, sdsRef, alertPublishBoltName, kafkaCluster, groupId, offsetRanges));
     }
 
-    private void fillInLatestOffsets() {
-        scala.collection.mutable.Set<String> mutableTopics = JavaConversions
-                .asScalaSet(this.topics);
-        scala.collection.immutable.Set<String> immutableTopics = mutableTopics
-                .toSet();
-        scala.collection.immutable.Set<TopicAndPartition> scalaTopicAndPartitionSet = kafkaCluster
-                .getPartitions(immutableTopics).right().get();
-        if (kafkaCluster.getConsumerOffsets(groupId,
-                scalaTopicAndPartitionSet).isLeft()) {
-            Set<TopicAndPartition> javaTopicAndPartitionSet = JavaConversions
-                    .setAsJavaSet(scalaTopicAndPartitionSet);
-            for (TopicAndPartition topicAndPartition : javaTopicAndPartitionSet) {
-                this.fromOffsets.put(topicAndPartition, 0L);
-            }
-        } else {
-            scala.collection.immutable.Map<TopicAndPartition, Object> consumerOffsetsTemp = kafkaCluster
-                    .getConsumerOffsets(groupId,
-                            scalaTopicAndPartitionSet).right().get();
-
-            Map<TopicAndPartition, Object> consumerOffsets = JavaConversions
-                    .mapAsJavaMap(consumerOffsetsTemp);
-            Set<TopicAndPartition> javaTopicAndPartitionSet = JavaConversions
-                    .setAsJavaSet(scalaTopicAndPartitionSet);
-            for (TopicAndPartition topicAndPartition : javaTopicAndPartitionSet) {
-                Long offset = (Long) consumerOffsets.get(topicAndPartition);
-                this.fromOffsets.put(topicAndPartition, offset);
-            }
-        }
+    private Set<String> getInitTopics(Config config) {
+        SpecMetadataServiceClientImpl client = new SpecMetadataServiceClientImpl(config);
+        return client.getSpoutSpec().getKafka2TupleMetadataMap().keySet();
     }
 
 }
---
+++ eagle-core/eagle-alert-parent/eagle-alert/alert-engine/src/main/java/org/apache/spark/broadcast/SpoutSpecData.java
@@ -0,0 +1,38 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ * <p/>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p/>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.spark.broadcast;
+
+import org.apache.eagle.alert.coordination.model.SpoutSpec;
+import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.broadcast.Broadcast;
+
+public class SpoutSpecData {
+    private static Broadcast<SpoutSpec> instance = null;
+
+    public static Broadcast<SpoutSpec> getInstance(JavaSparkContext jsc, final SpoutSpec meta) {
+        if (instance == null) {
+            synchronized (SpoutSpecData.class) {
+                if (instance == null) {
+                    instance = jsc.broadcast(meta);
+                }
+            }
+        }else{
+            instance = jsc.broadcast(meta);
+        }
+        return instance;
+    }
+}
